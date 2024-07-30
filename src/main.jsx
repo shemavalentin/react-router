176608a4a0847521dc08d297a4c38728a3923e0f
Shema N. Valentin <shemavalentin@gmail.com>
2024-07-30 16:44:31 +0200

    Added Link to to stop the client side to keep on requesting data from the server and added other API to load data and access it

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,11 @@ import {
 } from "react-router-dom";
 
 import './index.css';
-import Root from './routes/root';
+//import Root from './routes/root';
 import ErrorPage from './error-page';
+import Contact from './routes/contact';
+
+import Root,{loader as rootLoader } from "./routes/root"
 
 
 // Creating the root rout
@@ -16,8 +19,26 @@ const router = createBrowserRouter([
     path: '/',
     // Setting <Root> as the root route's element
     element: <Root />,
-    errorElement: <ErrorPage/>
+    errorElement: <ErrorPage />,
+
+    // using the loader
+    loader : rootLoader,
+
+    // making the contact route a child of the root route to be displayed into 
+    // the root layout frame
+
+    children: [  
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      }
+      // Now after making the components that we need to be displayed as children to the root layout
+      // we need also to tell the root layout where to display its children.
+
+      // this is where we add use <Outlet> in layout
+    ]   
   },
+
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,7 +1,31 @@
 
 // Creating the root layout component (the global layout)
 
+
+// importing Link for Cloud side routing to allow us to update the URL without
+// requesting another documrnt from the server. instead, the app can immediately
+// render new UI. this is done by using <Link> and removing <a href> to <Link to>
+
+import { Link, Outlet, useLoaderData } from "react-router-dom"; // added useLoaderData to access and render data
+
+// URL segments, layouts, and data are more often than not coupled(tripled).
+// because of this natural coupling, React route has data convention to get data into
+// our route components.
+
+// using the API: loader
+import { getContacts} from "../contacts"; 
+
+export async function loader() {
+    const contacts = await getContacts();
+    return { contacts };
+
+    // after this let's configure the loader on route in the main.js
+}
+
 export default function Root() {
+
+    // Accessing data 
+    const { contacts } = useLoaderData();
     return (
         <>
             <div id="sidebar">
@@ -31,19 +55,53 @@ export default function Root() {
                         <button type="submit">New</button>
                     </form>
                 </div>
-                <nav>
-                    <ul>
-                        <li>
-                            <a href={`/contact/1`}>Shema Valentin</a>
-                        </li>
 
-                        <li>
-                            <a href={`/contact/2`}>Your Friend</a>
-                        </li>
-                    </ul>
+
+                <nav>
+
+                    {/* displaying data */}
+
+                    {contacts.length ? (
+                        <ul>
+                            {contacts.map((contact) => (
+                            <li key={contact.id}>
+                                <Link to={`contacts/${contact.id}`}>
+                                {contact.first || contact.last ? (
+                                    <>
+                                    {contact.first} {contact.last}
+                                    </>
+                                ) : (
+                                    <i>No Name</i>
+                                )} {" "}
+                                {contact.favorite && <span>★</span>}
+                                </Link>
+                            </li>
+                            ))}
+                        </ul>
+                        ) : (
+                        <p>
+                            <i>No contacts</i>
+                        </p>
+                        )}
+
+
+                    {/* <ul> */}
+                        {/* <li> */}
+                            {/* <a href={`/contacts/1`}>Shema Valentin</a> */}
+                            {/* <Link to={`/contacts/1`}>Shema Valentin</Link> */}
+                        {/* </li> */}
+
+                        {/* <li> */}
+                            {/* <Link to ={`/contacts/2`}>My Friends</Link> */}
+                        {/* </li> */}
+                    {/* </ul> */}
                 </nav>
             </div>
-            <div id="detail"></div>
+            {/* adding <Outlet> in the div to tell the root layout where to display its child */}
+            <div id="detail">
+                <Outlet/>
+            </div>
         </>
+    
     );
 }
